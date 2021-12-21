b1461adbcf7ae07e49f840e29e54b0c7447d8158
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-21 19:39:08 +0100

    add rest endpoint for contact: get post delete

--- server/src/main.ts
+++ server/src/main.ts
@@ -29,7 +29,7 @@ app.get("/mailboxes",
             inResponse.send("error");
         }
     }
-)
+);
 
 app.get("/mailboxes/:mailbox",
     async (inRequest: Request, InResponse: Response) => {
@@ -80,6 +80,42 @@ app.post("/messages",
         try {
             const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
             await smtpWorker.sendMessage(inRequest.body);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.get("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contacts: IContact[] = await contactsWorker.listContacts();
+            inResponse.json(contacts);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/contacts", 
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contact: IContact = await contactsWorker.addContact(inRequest.body);
+            inResponse.json(contact); 
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/contacts/:id", 
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            await contactsWorker.deleteContact(inRequest.params.id);
             inResponse.send("ok");
         } catch (inError) {
             inResponse.send("error");

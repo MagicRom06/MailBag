e237066884450bb40cd93c4c48d77cd3c00e2480
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-21 18:16:05 +0100

    add cors headers params

--- server/dist/main.js
+++ server/dist/main.js
@@ -13,4 +13,10 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/", express_1.default.static(path_1.default.join(__dirname, "../../client/dist")));
+app.use((inRequest, inResponse, inNext) => {
+    inResponse.header("Access-Control-Allow-Origin", "*");
+    inResponse.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    inResponse.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    inNext();
+});
 //# sourceMappingURL=main.js.map
--- server/src/main.ts
+++ server/src/main.ts
@@ -12,3 +12,9 @@ app.use(express.json());
 app.use("/",
     express.static(path.join(__dirname, "../../client/dist"))
 );
+app.use((inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+    inResponse.header("Access-Control-Allow-Origin", "*");
+    inResponse.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    inResponse.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    inNext();
+});
